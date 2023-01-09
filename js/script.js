ba8b8e841bfadecddaf98d25a9ef6e38c1f59d3d
info-mic-info <jobsocial94@gmail.com>
2023-01-09 20:28:31 +0100

    Complete Fix 3

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-// const { createApp } = Vue
-
-// createApp({
-//     data() {
-//         return {
-//             message: 'Hello Vue!',
-//             Mail_List:[], 
-//             Single_Mail: null
-//         }
-//     },
-//     mounted() {
-//         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-//             this.Single_Mail = result.data.response
-
-//             console.log(this.Single_Mail)
-
-//             for (let i = 0; i < 5; i++) {
-//                 this.Mail_List.push(this.Single_Mail)
-//                 console.log(this.Mail_List)
-//             }
-
-//         })
-//     },
-// }).mount('#app')  
-
-
 const { createApp } = Vue
 
 createApp({
@@ -36,18 +10,13 @@ createApp({
         }
     },
     mounted() {
-        // axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-        //     this.Single_Mail = result.data.response
 
-                
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                     this.Mail_List.push(result.data.response)
                     
                 }
                 )}
-
-        // })
     },
     
 }).mount('#app')  
